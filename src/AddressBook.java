1ed3982e34399c78104993d831e1703203873d93
Oyindamola Taiwo-Olupeka <Olupekaoyinda@gmail.com>
2022-09-19 15:33:21 -0400

    I add some function calls to AddressBook.java to test Git Commit

---
+++ src/AddressBook.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class AddressBook {
+    private ArrayList<BuddyInfo> buddyInfo;
+
+    public void addBuddy(BuddyInfo buddyInfo1){
+       buddyInfo.add(buddyInfo1);
+    }
+
+    public void removeBuddy(){
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Address Book");
+    }
+}
